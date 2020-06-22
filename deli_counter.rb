1790ee01e5deecd6940b267938e61fdd2288cae9
An Pham <anpham148@gmail.com>
2020-06-22 12:20:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ katz_deli =[]
 def line(katz_deli)
   counter = 1
   if katz_deli.count == 0
-    str =  "The line is currently empty."
+    puts  "The line is currently empty."
   else
     str = "The line is currently:"
     katz_deli.each do |name|
