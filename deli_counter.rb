aa10f5683229c8c7747b959795fc82029de68c15
An Pham <anpham148@gmail.com>
2020-06-22 12:14:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ katz_deli =[]
 def line(katz_deli)
   counter = 1
   if katz_deli.count == 0
-    puts "The line is currently empty."
+    str =  "The line is currently empty."
   else
     katz_deli.each do |name|
       str = "The line is currently:"
